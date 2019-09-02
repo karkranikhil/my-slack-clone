20c25abffac1cdaadb002b494801b8502b93cdd3
nikhil karkra <karkra.nikhil@gmail.com>
2019-09-02 23:27:27 +1000

    login form and authentication

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,8 +1,93 @@
 import React, { Component } from 'react'
+import {Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react';
+import {Link} from 'react-router-dom'
+import firebase from '../../firebase'
+import * as CONSTANTS  from '../constants';
+
+ 
 class Login extends Component{
+    state={
+        email:'',
+        password:'',
+        errors:[],
+        loading:false,
+    }
+
+    /** Form fields change handler**/
+    handleChange=event=>{
+        this.setState({[event.target.name]:event.target.value})
+    }
+    
+    /** Form Submit handler**/
+    handleSubmit=event=>{
+        const {email, password}= this.state
+        event.preventDefault();
+        if(this.isFormValid(this.state)){
+            this.setState({errors:[], loading:true})
+            firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(singedInUser=>{
+                console.log(singedInUser)
+                this.setState({
+                    loading:false
+                })
+            }).catch(err=>{
+                console.error(err)
+                this.setState({
+                    errors:this.state.errors.concat(err),
+                    loading:false
+                })
+            })
+        }
+        
+    }
+
+    isFormValid=({email, password})=> email && password;
+
+    
+
+    /** display error for the  */
+    displayErrors =errors =>errors.map((error, i)=> <p key={i}>{error.message}</p>)
+
+
+    /** handle input error msg */
+
+    handleInputError =(errors, inputName)=>{
+        return errors.some(error=>error.message.toLowerCase().includes(inputName)) ? 'error': ''
+    }
     render(){
+        const  { email, password, errors, loading} = this.state
         return (
-            <div>Regsiter</div>
+            <Grid textAlign="center" verticalAlign="middle" className="app">
+                <Grid.Column style={{maxWidth:450}}>
+                    <Header as="h2" color="violet" textAlign="center">
+                        <Icon name="shield alternate" color="violet"/>
+                        Login to DevChat
+                    </Header>
+                    <Form size="large" autoComplete="off" onSubmit={this.handleSubmit}>
+                        <Segment stacked>
+                            <Form.Input  type="email" fluid name="email" icon="mail" iconPosition="left"
+                            className={this.handleInputError(errors, 'email')}
+                            placeholder="Email Address" onChange={this.handleChange} value={email}/>
+                            <Form.Input  type="password" fluid name="password" icon="lock" iconPosition="left"
+                            className={this.handleInputError(errors, 'password')}
+                            placeholder="Password" onChange={this.handleChange} value={password}/>
+
+                            <Button disabled={loading} className={loading? 'loading':''} color="violet" fluid size="large">Submit</Button>
+                        </Segment>
+                    </Form>
+                    {errors.length>0 && (
+                        <Message error>
+                            <h3>Error</h3>
+                            {this.displayErrors(errors)}
+
+                        </Message>
+                        
+                    )}
+                    <Message>Don't have an account?<Link to="/register"> Register</Link></Message>
+                </Grid.Column>
+            </Grid>
         )
     }
 }
--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth"
 import "firebase/database"
 import "firebase/storage"
 const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyCe3KPhJVC8UA6kBlDI_JdXxuJFfFozurg",
+    authDomain: "react-slack-clone-bd6b5.firebaseapp.com",
+    databaseURL: "https://react-slack-clone-bd6b5.firebaseio.com",
+    projectId: "react-slack-clone-bd6b5",
+    storageBucket: "react-slack-clone-bd6b5.appspot.com",
+    messagingSenderId: "124253278773",
+    appId: "1:124253278773:web:757b92bad4a91966"
   };
   firebase.initializeApp(config)
 
