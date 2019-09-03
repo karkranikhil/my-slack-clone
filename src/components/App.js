236e358d9bfdb484ad1eeef56bdcdd667e823ad5
nikhil karkra <karkra.nikhil@gmail.com>
2019-09-03 11:00:10 +1000

    basic layout design

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import {Grid} from 'semantic-ui-react'
 import './App.css';
+import ColorPanel from '../components/ColorPanel/ColorPanel'
+import SidePanel from '../components/SidePanel/SidePanel'
+import Messages from '../components/Messages/Messages'
+import MetaPanel from '../components/MetaPanel/MetaPanel'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        App
-      </div>
-    );
-  }
-}
-
-export default App;
+const App =()=>(
+  <Grid columns="equal" className="app" style={{background:"#eee"}}>
+    <ColorPanel/>
+    <SidePanel/>
+    <Grid.Column style={{marginLeft:320}}>
+      <Messages/>
+    </Grid.Column>
+    <Grid.Column width={4}>
+      <MetaPanel/>
+    </Grid.Column>
+  </Grid>
+)
+export default App
