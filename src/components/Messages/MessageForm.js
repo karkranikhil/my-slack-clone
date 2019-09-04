0cd2bad88cf51881e370d14449c7b193f7d177ac
nikhil karkra <karkra.nikhil@gmail.com>
2019-09-04 23:11:47 +1000

    direct message and file upload

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -39,11 +39,12 @@ class MessageForm extends Component{
         return message
     }
     sendMessage=()=>{
-        const {messagesRef} = this.props;
+        const {getMessagesRef} = this.props;
         const {message, channel} = this.state
         if(message){
             this.setState({loading:true})
-            messagesRef.child(channel.id)
+            getMessagesRef()
+            .child(channel.id)
             .push()
             .set(this.createMessage())
             .then(()=>{
@@ -61,11 +62,19 @@ class MessageForm extends Component{
             })
         }
     }
+    
+    getPath =()=>{
+        if(this.props.isPrivateChannel){
+            return `chat/private-${this.state.channel.id}`
+        } else {
+            return `chat/public`
+        }
+    }
     uploadFile=(file, metadata)=>{
         console.log(file)
         const pathToUpload = this.state.channel.id
-        const ref= this.props.messagesRef
-        const filePath = `chat/public/${uuidv4()}.jpg`
+        const ref= this.props.getMessagesRef()
+        const filePath = `${this.getPath()}/${uuidv4()}.jpg`
         this.setState({
             uploadState:'uploading',
             uploadTask:this.state.storageRef.child(filePath).put(file, metadata)
